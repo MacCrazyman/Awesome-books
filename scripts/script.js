06de04b55eb3d1b3fb47b5555a4bbceab31c40ed
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-11-22 14:59:51 -0500

    Add pail HTML

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+class Book {
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+  }
+
+}
+
+const addBook = (book) => {
+
+}
+
+const getBook = () => {
+
+}
+
+const displayBooks = () => {
+
+}
+
+const deleteBook = () => {
+
+}
+
+const removeBook = () => {
+
+}
+
+const createTable = () => {
+
+}
