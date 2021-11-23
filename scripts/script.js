51fdb3f5beefc19a00b2b49aca6a7c0198dd8e02
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-11-23 16:21:13 -0500

    Fix local storage retreive problem

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-class Book {
+class Book { // eslint-disable-line
   constructor(id, title, author) {
     this.id = id;
     this.title = title;
@@ -7,24 +7,19 @@ class Book {
 }
 
 class Library {
-  constructor(){
-     this.books = [];
-  }
-
-  getBook = () => {
-  
+  constructor() {
     if (localStorage.getItem('books') === null) {
       this.books = [];
     } else {
       this.books = JSON.parse(localStorage.getItem('books'));
     }
-  };
-  
+  }
+
   addBook = (book) => {
     this.books.push(book);
     localStorage.setItem('books', JSON.stringify(this.books));
   };
-  
+
   createTable = (book) => {
     const table = document.getElementById('table');
     const tr = document.createElement('div');
@@ -38,7 +33,7 @@ class Library {
 
   displayBooks = () => {
     this.books.forEach((book) => {
-      createTable(book);
+      this.createTable(book);
     });
   };
 
@@ -47,16 +42,14 @@ class Library {
   };
 
   removeBook = (id) => {
-    
     this.books = this.books.filter((value) => value.id !== id);
     localStorage.setItem('books', JSON.stringify(this.books));
   };
 }
 
-
 // create new book objects
-  const Storage = new Library ()
-  Storage.getBook()
+const Storage = new Library();
+
 document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
   const title = document.getElementById('title').value;
